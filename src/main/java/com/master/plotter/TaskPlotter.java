a34e2840db461598fa61abd965f88639f5462c89
nikitaln <nikitaln7@gmail.com>
2024-07-31 16:24:39 +0300

    completed parsing web page

--- src/main/java/com/master/plotter/TaskPlotter.java
+++ src/main/java/com/master/plotter/TaskPlotter.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -20,101 +21,220 @@ import java.util.List;
 
 public class TaskPlotter {
 
-    private String startDate = "15.07.2024";
-    private String endDate = "21.07.2024";
+    private String taskName;
+    private String taskType;
+    private String source;
+    private String output;
+    private String status;
+    private int copyCount;
+    private String paperType;
+    private double paperConsumption;
+    private double paperLengthConsumption;
+    private double tonerConsumption;
+    private String user;
+    private LocalDateTime dateTime;
+    private String printType;
 
-    public void parsingTxtFile(String path) {
+    public String getTaskName() {
+        return taskName;
+    }
 
-        double allLengthPaper = 0;
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
 
-        LocalDate startLocalDate = getLocalDateFromString(startDate);
-        LocalDate endLocalDate = getLocalDateFromString(endDate);
+    public String getTaskType() {
+        return taskType;
+    }
 
-        File file = new File(path);
-        System.out.println(file.length());
-        Path filePath = Paths.get(path);
+    public void setTaskType(String taskType) {
+        this.taskType = taskType;
+    }
 
-        try {
+    public String getSource() {
+        return source;
+    }
 
-            List<String> lines = Files.readAllLines(filePath);
+    public void setSource(String source) {
+        this.source = source;
+    }
 
-            for (String line : lines) {
+    public String getOutput() {
+        return output;
+    }
 
-                if (line.contains("HP SmartStream") && !line.contains("0,0000")) {
+    public void setOutput(String output) {
+        this.output = output;
+    }
 
-                    String[] fragments = line.split(";");
-                    // [0] - имя документа
-                    // [1] - тип задания (печать)
-                    // [2] - источник задания
-                    // [3] - конечный выход (укладчик и тд)
-                    // [4] - состояние (отпечатано)
-                    // [5] - копии
-                    // [6] - тип затрат
-                    // [7] - значение затрат
-                    // [8] - тип бумаги
-                    // [9] - расход бумаги
-                    // [10] - длина бумаги
-                    // [11] - длина сканирования
-                    // [12] - одна категория
-                    // [13] - использованные чернила
-                    // [14] - имя пользователя
-                    // [15] - дата печати
-                    // [15] - качество печати
+    public String getStatus() {
+        return status;
+    }
 
-//                    System.out.println(
-//                            fragments[0] + "\t" +
-//                            fragments[1] + "\t" +
-//                            fragments[2] + "\t" +
-//                            fragments[3] + "\t" +
-//                            fragments[4] + "\t" +
-//                            fragments[5] + "\t" +
-//                            fragments[6] + "\t" +
-//                            fragments[7] + "\t" +
-//                            fragments[8] + "\t" +
-//                            fragments[9] + "\t" +
-//                            fragments[10] + "\t" +
-//                            fragments[11] + "\t" +
-//                            fragments[12] + "\t" +
-//                            fragments[13] + "\t" +
-//                            fragments[14] + "\t" +
-//                            fragments[15] + "\t" +
-//                            fragments[16] + "\t" +
-//                            fragments[17] + "\t");
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-                    String date = getStringDateWithoutTime(fragments[15]);
-                    LocalDate localDate = getLocalDateFromString(date);
+    public int getCopyCount() {
+        return copyCount;
+    }
 
-                    if (fragments[8].contains("Обычная") &&
-                            startLocalDate.minusDays(1).isBefore(localDate) &&
-                            endLocalDate.plusDays(1).isAfter(localDate)) {
+    public void setCopyCount(int copyCount) {
+        this.copyCount = copyCount;
+    }
 
-                        String stringLengthPaper = fragments[10].replace(",",".");
-                        double doubleLengthPaper = Double.valueOf(stringLengthPaper);
-                        allLengthPaper = allLengthPaper + doubleLengthPaper;
-                        System.out.println(fragments[15]);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (IndexOutOfBoundsException e) {
+    public String getPaperType() {
+        return paperType;
+    }
 
-        }
+    public void setPaperType(String paperType) {
+        this.paperType = paperType;
+    }
 
-        System.out.println("Итого - " + allLengthPaper);
+    public double getPaperConsumption() {
+        return paperConsumption;
+    }
+
+    public void setPaperConsumption(double paperConsumption) {
+        this.paperConsumption = paperConsumption;
+    }
+
+    public double getPaperLengthConsumption() {
+        return paperLengthConsumption;
+    }
+
+    public void setPaperLengthConsumption(double paperLengthConsumption) {
+        this.paperLengthConsumption = paperLengthConsumption;
+    }
+
+    public double getTonerConsumption() {
+        return tonerConsumption;
+    }
+
+    public void setTonerConsumption(double tonerConsumption) {
+        this.tonerConsumption = tonerConsumption;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getPrintType() {
+        return printType;
+    }
+
+    public void setPrintType(String printType) {
+        this.printType = printType;
     }
 
 
-    //example input - 25.01.2024
-    public LocalDate getLocalDateFromString(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        return LocalDate.parse(date, formatter);
-    }
-
-
-
-    public String getStringDateWithoutTime(String dateAndTime) {
-        String[] dateTime = dateAndTime.split("\s");
-        return dateTime[0];
-    }
+    //    private String startDate = "15.07.2024";
+//    private String endDate = "21.07.2024";
+//
+//    public void parsingTxtFile(String path) {
+//
+//        double allLengthPaper = 0;
+//
+//        LocalDate startLocalDate = getLocalDateFromString(startDate);
+//        LocalDate endLocalDate = getLocalDateFromString(endDate);
+//
+//        File file = new File(path);
+//        System.out.println(file.length());
+//        Path filePath = Paths.get(path);
+//
+//        try {
+//
+//            List<String> lines = Files.readAllLines(filePath);
+//
+//            for (String line : lines) {
+//
+//                if (line.contains("HP SmartStream") && !line.contains("0,0000")) {
+//
+//                    String[] fragments = line.split(";");
+//                    // [0] - имя документа
+//                    // [1] - тип задания (печать)
+//                    // [2] - источник задания
+//                    // [3] - конечный выход (укладчик и тд)
+//                    // [4] - состояние (отпечатано)
+//                    // [5] - копии
+//                    // [6] - тип затрат
+//                    // [7] - значение затрат
+//                    // [8] - тип бумаги
+//                    // [9] - расход бумаги
+//                    // [10] - длина бумаги
+//                    // [11] - длина сканирования
+//                    // [12] - одна категория
+//                    // [13] - использованные чернила
+//                    // [14] - имя пользователя
+//                    // [15] - дата печати
+//                    // [15] - качество печати
+//
+////                    System.out.println(
+////                            fragments[0] + "\t" +
+////                            fragments[1] + "\t" +
+////                            fragments[2] + "\t" +
+////                            fragments[3] + "\t" +
+////                            fragments[4] + "\t" +
+////                            fragments[5] + "\t" +
+////                            fragments[6] + "\t" +
+////                            fragments[7] + "\t" +
+////                            fragments[8] + "\t" +
+////                            fragments[9] + "\t" +
+////                            fragments[10] + "\t" +
+////                            fragments[11] + "\t" +
+////                            fragments[12] + "\t" +
+////                            fragments[13] + "\t" +
+////                            fragments[14] + "\t" +
+////                            fragments[15] + "\t" +
+////                            fragments[16] + "\t" +
+////                            fragments[17] + "\t");
+//
+//                    String date = getStringDateWithoutTime(fragments[15]);
+//                    LocalDate localDate = getLocalDateFromString(date);
+//
+//                    if (fragments[8].contains("Обычная") &&
+//                            startLocalDate.minusDays(1).isBefore(localDate) &&
+//                            endLocalDate.plusDays(1).isAfter(localDate)) {
+//
+//                        String stringLengthPaper = fragments[10].replace(",",".");
+//                        double doubleLengthPaper = Double.valueOf(stringLengthPaper);
+//                        allLengthPaper = allLengthPaper + doubleLengthPaper;
+//                        System.out.println(fragments[15]);
+//                    }
+//                }
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } catch (IndexOutOfBoundsException e) {
+//
+//        }
+//
+//        System.out.println("Итого - " + allLengthPaper);
+//    }
+//
+//
+//    //example input - 25.01.2024
+//    public LocalDate getLocalDateFromString(String date) {
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+//        return LocalDate.parse(date, formatter);
+//    }
+//
+//
+//
+//    public String getStringDateWithoutTime(String dateAndTime) {
+//        String[] dateTime = dateAndTime.split("\s");
+//        return dateTime[0];
+//    }
 }
--- src/main/java/com/master/plotter/TaskPlotterService.java
+++ src/main/java/com/master/plotter/TaskPlotterService.java
@@ -3,34 +3,167 @@ package com.master.plotter;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaskPlotterService {
 
+    private TaskPlotter taskPlotter;
+
+
     //парсинг веб-страницы HP
     public void parseWebPrinterStatistics(String path) {
 
-        Connection connection = Jsoup.connect("http://npic5e08b/hp/device/webAccess/index.htm?content=accounting");
+        Connection connection = Jsoup.connect("http://npic5e08b/hp/device/webAccess/index.htm;jsessionid=blir95p7c1?content=accounting").maxBodySize(Integer.MAX_VALUE);
+
         try {
-            AtomicInteger i = new AtomicInteger();
+
             Document document = connection.get();
-            Elements elements = document.select("tr");
-            elements.forEach(element -> {
+            Elements table = document.select("table").get(4).getElementsByClass("treeTableTitleRowClosed treeTableTopLevel");
 
-                if (element.hasClass("treeTableTitleRowClosed treeTableTopLevel")) {
+            //Elements elementsRows = table.select("tr");
 
-                    System.out.println(i + " " + element.text());
-                    i.set(i.get() + 1);
-                }
-
-
-            });
+            //создание объектов Задачи
+            for (Element row : table) {
+                Elements cols = row.select("td");
+                createTaskPlotterObject(cols);
+//                System.out.println(cols.get(0).text()); //имя файла
+//                System.out.println(cols.get(1).text()); //тип задания
+//                System.out.println(cols.get(2).text()); //источник задания
+//                System.out.println(cols.get(3).text()); //выход задания на печать
+//                System.out.println(cols.get(4).text()); //состояние (отпечатано)
+//                System.out.println(cols.get(5).text()); //копии
+//                System.out.println(cols.get(6).text()); //тип затрат (НЕ РАБОТАЕТ)
+//                System.out.println(cols.get(7).text()); //тип затрат (Всего)
+//                System.out.println(cols.get(8).text()); //НЕ РАБОТАЕТ
+//                System.out.println(cols.get(9).text()); //значение затрат
+//                System.out.println(cols.get(10).text());    //тип бумаги
+//                System.out.println(cols.get(11).text());    //расход бумаги
+//                System.out.println(cols.get(12).text());    //длина бумаги
+//                System.out.println(cols.get(13).text());    //одна категория
+//                System.out.println(cols.get(14).text());    //использование чернил
+//                System.out.println(cols.get(15).text());    //имя пользователя
+//                System.out.println(cols.get(16).text());    //дата
+//                System.out.println(cols.get(17).text());    //качество печати
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+
+
+    public TaskPlotter createTaskPlotterObject(Elements cols) {
+
+        taskPlotter = new TaskPlotter();
+        taskPlotter.setTaskName(cols.get(0).text());
+        taskPlotter.setTaskType(cols.get(1).text());
+        taskPlotter.setSource(cols.get(2).text());
+        taskPlotter.setOutput(cols.get(3).text());
+        taskPlotter.setStatus(cols.get(4).text());
+        taskPlotter.setCopyCount(Integer.valueOf(cols.get(5).text()));
+        taskPlotter.setPaperType(cols.get(10).text());
+        taskPlotter.setPaperConsumption(
+                getPaperConsumptionFromString(cols.get(11).text()));
+
+        taskPlotter.setPaperLengthConsumption(
+                getPaperLengthConsumptionFromString(cols.get(12).text()));
+
+        taskPlotter.setTonerConsumption(
+                getTonerConsumptionFromString(cols.get(14).text()));
+
+        taskPlotter.setUser(cols.get(15).text());
+        taskPlotter.setDateTime(
+                getLocalDateTimeFromString(cols.get(16).text()));
+        taskPlotter.setPaperType(cols.get(17).text());
+
+        return taskPlotter;
+    }
+
+
+
+    public double getPaperConsumptionFromString(String paperSquare) {
+        //input - 1,0710 м²
+        //output - 1,0710
+
+        return 0.0;
+    }
+
+
+
+    public double getPaperLengthConsumptionFromString(String paperLength) {
+        //input - 1,2700 м
+        //output - 1,2700
+        return 0.0;
+    }
+
+
+
+    public double getTonerConsumptionFromString(String toner) {
+        //input - 1,54 мл.
+        //output - 1,54
+        return 0.0;
+    }
+
+
+
+    public LocalDateTime getLocalDateTimeFromString(String dateTime) {
+        //18.07.2024 16:08:11
+        return null;
+    }
+
+
+    //Первый способ
+    public void parseWebPrinterStatisticsWithoutUseTable() {
+
+        Connection connection = Jsoup.connect("http://npic5e08b/hp/device/webAccess/index.htm;jsessionid=blir95p7c1?content=accounting").maxBodySize(Integer.MAX_VALUE);
+        try {
+            AtomicInteger i = new AtomicInteger();
+            AtomicInteger j = new AtomicInteger();
+            Document document = connection.get();
+
+            Elements elements = document.select("tr");
+            elements.forEach(element -> {
+
+                if (element.hasClass("treeTableTitleRowClosed treeTableTopLevel")) {
+
+                    String[] array = new String[15];
+
+                    i.set(i.get() + 1);
+                    if (i.get() == 1) {
+
+                        element.select("tr.treeTableTopLevel td").forEach(element1 -> {
+                                    if (!element1.text().isEmpty()) {
+                                        //0 ВАО_А1_1копия_БезФальц_ОбычнаяБумага_реновация2
+                                        //1 Печать
+                                        //2 HP SmartStream
+                                        //3 Укладчик
+                                        //4 отменено пользователем
+                                        //5 0
+                                        //6 Всего
+                                        //7 0
+                                        //8 Обычная бумага (< 90 г/кв.м)
+                                        //9 1,0710 м²
+                                        //10 1,2700 м
+                                        //11 1,54 мл.
+                                        //12 Biegler
+                                        //13 18.07.2024 16:08:11
+                                        //14 Линии/быстро
+                                        array[j.get()] = element1.text();
+                                        System.out.println(j + " " + element1.text());
+                                        j.set(j.get() + 1);
+                                    }
+                                }
+                        );
+                    }
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
